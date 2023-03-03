bdc801fde049368c19d7627c0f3e45cef8ac0f99
jangamsrikanth <101241702+jangamsrikanth@users.noreply.github.com>
2023-03-03 20:37:41 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -14,7 +14,7 @@ resource "azurerm_virtual_machine" "Sonar004" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
   storage_os_disk {
